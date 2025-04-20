2a23d3a7dbd32e731b4ebdb1a4a626eec3bf582c
Salma WADOUACHI <wadouachisalma@gmail.com>
2025-04-20 19:30:51 +0200

    fix: clean code

--- shotgun/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://thtiqavkmsgftlztikre.supabase.co' 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRodGlxYXZrbXNnZnRsenRpa3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMDc5MDMsImV4cCI6MjA2MDU4MzkwM30.nLQCC5JGyUI6Yb2v1HNwaqqPCULJx9iXE-vRHkHdbA4' 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
